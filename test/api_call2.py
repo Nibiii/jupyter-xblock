12ae8d21687eee58cf97925a04bbf2560ef07ebf
dominic <dominic@hubblestudios.com>
2016-04-13 13:37:08 +0200

    Move the temporary python api calls into test folder

--- test/api_call2.py
+++ test/api_call2.py
@@ -7,7 +7,7 @@ payload = "{\"admin\":false,\"usernames\":[\"new_unix_user\"]}"
 headers = {
 		'referer': "127.0.0.1:8081/hub/",
     'content-type': "application/json",
-    'cookie': "jupyter-hub-token=\"2|1:0|10:1460464624|17:jupyter-hub-token|44:ZjZmYzUzMTRlMjgzNDE0MzkzYjcwODQ2YjEyN2I4YjY=|9f2e40c810b8fb1744cb4e203bec24ea94f3cb82d75b3d097419aade3dbd10a7\""
+    'authorization': "Basic ZWR4X3hibG9ja19qdXB5dGVyOmVkeA==",
     }
 
 conn.request("POST", "/hub/api/users", payload, headers)
--- test/api_call3.py
+++ test/api_call3.py
@@ -2,14 +2,13 @@ import http.client
 
 conn = http.client.HTTPConnection("127.0.0.1:8081")
 
-payload = "{\"admin\":false,\"usernames\":[\"edx_xblock_jupyter\"]}"
-
 headers = {
     'referer': "127.0.0.1:8081/hub/",
     'content-type': "application/json",
+    'authorization': "Basic ZWR4X3hibG9ja19qdXB5dGVyOmVkeA==",
     }
 
-conn.request("GET", "/hub/api/authorizations/token/2514fff9f3fc42d730c09ba60a33a373d9e3b8e1c7e12fc20893b53427b12084", payload, headers)
+conn.request("GET", "/hub/api/authorizations/token/2514fff9f3fc42d730c09ba60a33a373d9e3b8e1c7e12fc20893b53427b12084", headers=headers)
 
 res = conn.getresponse()
 data = res.read()
